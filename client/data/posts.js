e55ec013f37df2a6cfeda28561c1f9344cbb4c3d
TovarishFin <tovarishfin@gmail.com>
2017-08-18 00:20:24 +0200

    removed photos

--- client/data/posts.js
+++ client/data/posts.js
@@ -56,20 +56,6 @@ const posts = [
       "display_src":"https://scontent.cdninstagram.com/hphotos-xap1/t51.2885-15/e35/1516572_445736812276082_2116173059_n.jpg"
    },
    {
-      "code":"_4jHytwcUA",
-      "caption":"2015 can be summed up with one baby and a many lines of code. And sometimes a coding baby. 👶🏼⌨",
-      "likes":62,
-      "id":"1150824171912152320",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/e35/10723795_1149927178351091_1859033096_n.jpg"
-   },
-   {
-      "code":"_zbaOlQcbn",
-      "caption":"Lekker Chocoladeletter",
-      "likes":52,
-      "id":"1149382879529256679",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xfp1/t51.2885-15/e35/12346073_1035047523184672_768982339_n.jpg"
-   },
-   {
       "code":"_rmvQfQce8",
       "caption":"Just discovered the #hamont farmers market has a new ramen place! 🍜",
       "likes":35,
@@ -77,25 +63,11 @@ const posts = [
       "display_src":"https://scontent.cdninstagram.com/hphotos-xft1/t51.2885-15/e35/12331739_1671776806423597_995664526_n.jpg"
    },
    {
-      "code":"_ep9kiQcVy",
-      "caption":"⛄️",
-      "likes":64,
-      "id":"1143535906423162226",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xft1/t51.2885-15/e35/12354078_447337935474115_1484398925_n.jpg"
-   },
-   {
       "code":"_XpJcrwcSn",
       "caption":"6 page spread on flexbox in this months netmag!",
       "likes":74,
       "id":"1141561999742846119",
       "display_src":"https://scontent.cdninstagram.com/hphotos-xfp1/t51.2885-15/e35/12362588_1688046211438811_1395882545_n.jpg"
-   },
-   {
-      "code":"_KnU7MwceA",
-      "caption":"Hanging out in my office waiting for 5:00 beers to come around.",
-      "likes":54,
-      "id":"1137894817632733056",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xfp1/t51.2885-15/e35/12301208_1533749386944985_1334730917_n.jpg"
    },
    {
       "code":"_HMejJQcY5",
@@ -126,13 +98,6 @@ const posts = [
       "display_src":"https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/e35/12331333_1650987978502155_1162510634_n.jpg"
    },
    {
-      "code":"-pjx-gQcVi",
-      "caption":"My baby and me. Thanks to @bearandsparrow for this one.",
-      "likes":81,
-      "id":"1128590547628442978",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xtf1/t51.2885-15/e35/12298962_863814057068027_460827278_n.jpg"
-   },
-   {
       "code":"-oTZ0zQcWt",
       "caption":"It's too early. Send coffee.",
       "likes":81,
@@ -140,32 +105,11 @@ const posts = [
       "display_src":"https://scontent.cdninstagram.com/hphotos-xtf1/t51.2885-15/e35/12328347_990748230999662_1512917342_n.jpg"
    },
    {
-      "code":"-mxKQoQcQh",
-      "caption":"They both have figured it out. #lifewithsnickers",
-      "likes":47,
-      "id":"1127804966031967265",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xtp1/t51.2885-15/e35/12256736_1758525004381641_1136705181_n.jpg"
-   },
-   {
-      "code":"-fasqlQceO",
-      "caption":"Kaitlin decorated the house for the Christmas. So gezellig! #casabos",
-      "likes":46,
-      "id":"1125735850454402958",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xpt1/t51.2885-15/e35/12277581_1028556737218368_1184190781_n.jpg"
-   },
-   {
       "code":"-VBgtGQcSf",
       "caption":"Trying the new Hamilton Brewery beer. Big fan.",
       "likes":27,
       "id":"1122810327591928991",
       "display_src":"https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/e35/12224456_175248682823294_1558707223_n.jpg"
-   },
-   {
-      "code":"-FpTyHQcau",
-      "caption":"I'm in Austin for a conference and doing some training. Enjoying some local brew with my baby.",
-      "likes":82,
-      "id":"1118481761857291950",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xpt1/t51.2885-15/e35/11326072_550275398458202_1726754023_n.jpg"
    }
 ];
 
